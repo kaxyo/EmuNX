5e25d88496c58b1fbd1aa21b8ed4949b5efe6010
kaxyo <97456541+kaxyo@users.noreply.github.com>
2025-05-06 19:22:09 +0200

    Feat(Prototype): Open PFS from FileDialog selection

--- Src/HelloWorld/HelloWorld.cs
+++ Src/HelloWorld/HelloWorld.cs
@@ -1,6 +1,8 @@
-using System;
 using Godot;
 
+using LibHac;
+using LibHac.Common;
+using LibHac.Fs.Fsa;
 using LibHac.FsSystem;
 
 public partial class HelloWorld : Control
@@ -18,7 +20,19 @@ public partial class HelloWorld : Control
 
     private void OnFileSelected(string path)
     {
-        string globalizedPath = ProjectSettings.GlobalizePath(path); 
+        string globalizedPath = ProjectSettings.GlobalizePath(path);
         GD.Print(globalizedPath);
+
+        using var file = new LocalStorage(globalizedPath, System.IO.FileAccess.Read);
+
+        IFileSystem fs = null;
+        using UniqueRef<PartitionFileSystem> pfs = new UniqueRef<PartitionFileSystem>();
+        using UniqueRef<Sha256PartitionFileSystem> hfs = new UniqueRef<Sha256PartitionFileSystem>();
+
+        pfs.Reset(new PartitionFileSystem());
+        Result result = pfs.Get.Initialize(file);
+
+        GD.Print(result.IsSuccess() ? "NSP has been open!" : "Couldn't open NSP");
+        if (!result.IsSuccess()) return;
     }
 }
