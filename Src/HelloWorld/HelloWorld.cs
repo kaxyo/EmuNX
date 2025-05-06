072e8aa9b5ad8895039b5b92dc5197a368255b8f
kaxyo <97456541+kaxyo@users.noreply.github.com>
2025-05-06 18:55:20 +0200

    Feat(Prototype): Open file dialog to ask ROM path

--- Src/HelloWorld/HelloWorld.cs
+++ Src/HelloWorld/HelloWorld.cs
@@ -1,9 +1,24 @@
+using System;
 using Godot;
 
-public partial class HelloWorld : Node2D
+using LibHac.FsSystem;
+
+public partial class HelloWorld : Control
 {
+    private FileDialog fileDialog;
+
     public override void _Ready()
     {
         GD.Print("Hello from C# to Godot :)");
+
+        fileDialog = GetNode<FileDialog>("FileDialog");
+        fileDialog.FileSelected += OnFileSelected;
+        fileDialog.Popup();
+    }
+
+    private void OnFileSelected(string path)
+    {
+        string globalizedPath = ProjectSettings.GlobalizePath(path); 
+        GD.Print(globalizedPath);
     }
 }
