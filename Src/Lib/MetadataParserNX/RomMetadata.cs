08ca662c9618d0d06aaed92e8b3d601350cd23aa
kaxyo <97456541+kaxyo@users.noreply.github.com>
2025-06-21 12:34:11 +0200

    Feat(Lib): Created class that stores the basic metadata for a ROM

---
+++ Src/Lib/MetadataParserNX/RomMetadata.cs
@@ -0,0 +1,60 @@
+namespace EmuNX.Lib.MetadataParserNX;
+
+using System;
+using System.IO;
+
+/// <summary>
+/// Contains the basic metadata of a ROM.
+/// </summary>
+public class RomMetadata : IDisposable
+{
+    public string Name;
+
+    private ulong _id;
+    public ulong Id
+    {
+        get => _id;
+        set
+        {
+            _id = value;
+            IdString = $"{_id:X}";
+        }
+    }
+
+    public string IdString { get; private set; }
+
+    private MemoryStream _icon;
+    public Stream Icon
+    {
+        get => _icon;
+        private set
+        {
+            DisposeIcon();
+            if (value == null) return;
+            _icon = new MemoryStream();
+            value.CopyTo(_icon);
+            _icon.Position = 0;
+        }
+        
+    }
+
+    public RomMetadata() : this(null, 0, null) {}
+    
+    public RomMetadata(string name, ulong id, Stream icon)
+    {
+        Name = name;
+        Id = id;
+        Icon = icon;
+    }
+
+    private void DisposeIcon()
+    {
+        _icon?.Dispose();
+        _icon = null;
+    }
+    
+    public void Dispose()
+    {
+        DisposeIcon();
+    }
+}
