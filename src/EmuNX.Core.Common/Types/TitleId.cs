29f05e0748846407374350ddbab23bf406c74068
kaxyo <97456541+kaxyo@users.noreply.github.com>
2025-07-30 18:53:45 +0200

    Feat: Created TitleId class to store that number and also provide utility methods

---
+++ src/EmuNX.Core.Common/Types/TitleId.cs
@@ -0,0 +1,33 @@
+namespace EmuNX.Core.Common.Types;
+
+/// <summary>
+/// Stores a ROM title identifier. Provides utility  methods. 
+/// </summary>
+public class TitleId
+{
+    private readonly ulong Num;
+
+    public TitleId(ulong num)
+    {
+        Num = num;
+    }
+
+    public TitleId(string hex)
+    {
+        if (!ulong.TryParse(hex, System.Globalization.NumberStyles.HexNumber, null, out Num))
+        {
+            throw new ArgumentException("Invalid hexadecimal format for TitleId.");
+        }
+    }
+
+    public string Hex => Num.ToString("X16");
+
+    public override string ToString() => Hex;
+
+    public override bool Equals(object? obj)
+    {
+        return obj is TitleId other && Num == other.Num;
+    }
+
+    public override int GetHashCode() => Num.GetHashCode();
+}
