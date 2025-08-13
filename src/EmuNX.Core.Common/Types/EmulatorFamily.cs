30aeacc46bde5aedbb8ed752adb8714378ea4e80
kaxyo <97456541+kaxyo@users.noreply.github.com>
2025-08-13 22:45:36 +0200

    Feat: Added extension methods to EmulatorFamily

--- src/EmuNX.Core.Common/Types/EmulatorFamily.cs
+++ src/EmuNX.Core.Common/Types/EmulatorFamily.cs
@@ -5,3 +5,26 @@ public enum EmulatorFamily
     Yuzu,
     Ryujinx,
 }
+
+public static class EmulatorFamilyExtensions
+{
+    public static string ToString(this EmulatorFamily value)
+    {
+        return value switch
+        {
+            EmulatorFamily.Yuzu => "yuzu",
+            EmulatorFamily.Ryujinx => "ryujinx",
+            _ => "yuzu"
+        };
+    }
+    
+    public static EmulatorFamily? FromString(string family)
+    {
+        return family.ToLower() switch
+        {
+            "yuzu" => EmulatorFamily.Yuzu,
+            "ryujinx" => EmulatorFamily.Ryujinx,
+            _ => null
+        };
+    }
+}
