93dd17bf3525ea7432a0597444e24d5016a171bf
kaxyo <97456541+kaxyo@users.noreply.github.com>
2025-08-19 21:04:34 +0200

    Feat: Added ResourceDeserializationFailed error to LoadError

--- src/EmuNX.Core.Configuration/TitleExecutionPetition/IO/Errors.cs
+++ src/EmuNX.Core.Configuration/TitleExecutionPetition/IO/Errors.cs
@@ -3,10 +3,10 @@ namespace EmuNX.Core.Configuration.TitleExecutionPetition.IO;
 /// Errors that might arise when <b>loading</b> a <see cref="TepConfig"/>.
 public enum LoadError
 {
-    /// This should never arise as it is a placeholder.
-    Unknown,
     /// The resource (e.g. file) couldn't be read.
-    ResourceReadFailed,
+    ResourceAccessFailed,
+    /// The resource (e.g. file) couldn't be deserialized.
+    ResourceDeserializationFailed,
     /// A <see cref="Version"/> couldn't be found in the resource.
     MetaVersionNotFound,
     /// The <see cref="Version"/> found in the resource is not compatible.
